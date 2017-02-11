cc7d78fcfd6a8c8627f33dc354a558ad55759606
Wahid Shafique <wahidshafique@hotmail.com>
2017-02-11 16:36:16 -0500

    fixe up some of the scaler ray stuff

--- Assets/Wahid/Scripts/ShootCast.cs
+++ Assets/Wahid/Scripts/ShootCast.cs
@@ -2,70 +2,36 @@ using UnityEngine;
 using System.Collections;
 
 public class ShootCast : MonoBehaviour {
-    public int sizeRatio = 1;
-    public float weaponRange;
+    public float weaponShootRange;
     public Transform gunEnd;
 
+    private bool stuck = false;//check if you haved hooked to an object
+    private bool canFire = true;
     private Camera fpsCam;
-    private WaitForSeconds coolDown = new WaitForSeconds(0.5f);
     private LineRenderer laserLine;
-    private float nextFire;
-    private Shader highLight;
     private GameObject currentHit;
     private Color currentColor;
-    private bool stuck = false;//see if raycast should be set to dir of obj
-    private bool canFire = true;
+    private Vector3 endPos;
     [SerializeField]
     private LayerMask mask1;
-    private const int mask = 1 << 9;//this is the layer mask to ignore (ignores player)
-    private Vector3 endPos;
+
+    public delegate void Scaler(GameObject param);
 
     void Start() {
         laserLine = GetComponent<LineRenderer>();
         fpsCam = GetComponentInParent<Camera>();
-        highLight = Shader.Find("Self-Illumin/Outlined Diffuse");
     }
 
     void Update() {
         if (canFire) {
             if (Input.GetButton("Fire1")) {
-                Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
-                RaycastHit normalhit;
-                laserLine.SetPosition(0, gunEnd.position);
-
-                if (!stuck) {
-                    if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out normalhit, weaponRange)) {
-                        Debug.DrawRay(rayOrigin, fpsCam.transform.forward * weaponRange, Color.red);
-                        if (normalhit.transform.gameObject.CompareTag("Sizeable")) {
-                            stuck = true;
-                            currentHit = normalhit.transform.gameObject;
-                            //first set the raycast to the pos of object [lock it]
-                            currentColor = currentHit.GetComponent<Renderer>().material.color;
-                            currentHit.GetComponent<Renderer>().material.color = new Color(100, 140, 0);
-                            laserLine.enabled = true;
-                            endPos = currentHit.transform.GetComponent<Renderer>().bounds.center;
-                        }
-                    }
-                } else {
-                    //if you have stuck, draw line to that object
-                    laserLine.SetPosition(1, endPos);
-                    Debug.DrawLine(rayOrigin, currentHit.transform.position, Color.green);
-                    if (Physics.Linecast(rayOrigin, endPos, out normalhit, mask1)) {
-                        print(normalhit.collider.gameObject);
-                        if (normalhit.collider.gameObject.name != currentHit.name)
-                        {
-                            print(normalhit.collider.gameObject);
-                            if (normalhit.collider.gameObject != currentHit)
-                            {
-                                print("blocked");
-                                canFire = false;
-                            }
-                        }
-                    }
-                }
+                shootRay(growObject);
+            }
+            if (Input.GetButton("Fire2")) {
+                shootRay(shrinkObject);
             }
         }
-        if (Input.GetButtonUp("Fire1") || !canFire) {
+        if (Input.GetButtonUp("Fire1") || Input.GetButtonUp("Fire2") || !canFire) {
             laserLine.enabled = false;
             if (stuck || !canFire) {
                 currentHit.GetComponent<Renderer>().material.color = currentColor;
@@ -74,5 +40,52 @@ public class ShootCast : MonoBehaviour {
                 canFire = true;
             }
         }
+    }
+
+    void shootRay(Scaler scalerDel) {
+        Vector3 camCenter = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+        Vector3 rayOrigin = camCenter;
+        RaycastHit normalhit;
+        //lines 'resting' position
+        laserLine.SetPosition(0, gunEnd.position);
+        laserLine.SetPosition(1, camCenter);
+        if (!stuck) {
+            if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out normalhit, weaponShootRange)) {
+                Debug.DrawRay(rayOrigin, fpsCam.transform.forward * weaponShootRange, Color.red);
+                if (normalhit.transform.gameObject.CompareTag("Sizeable")) {
+                    stuck = true;
+                    currentHit = normalhit.transform.gameObject;
+                    //first set the raycast to the pos of object [lock it]
+                    currentColor = currentHit.GetComponent<Renderer>().material.color;
+                    currentHit.GetComponent<Renderer>().material.color = new Color(0, 0, 255);
+                    laserLine.enabled = true;
+                }
+            } else {
+                print("too far");
+            }
+        } else {
+            //if you are hooked to an object, draw line to that object
+            endPos = currentHit.transform.GetComponent<Renderer>().bounds.center;
+            laserLine.SetPosition(1, endPos);
+            this.transform.Rotate(new Vector3(0, 0, 60 * Time.deltaTime));
+            Debug.DrawLine(rayOrigin, currentHit.transform.position, Color.green);
+            if (Physics.Linecast(rayOrigin, endPos, out normalhit, mask1)) {
+                if (normalhit.collider.gameObject != currentHit) {
+                    print(normalhit.collider.gameObject);
+                    print("blocked");
+                    canFire = false;
+                } else {
+                    scalerDel(currentHit);
+                }
+            }
+        }
+    }
+
+    void growObject(GameObject otherObject) {
+        otherObject.transform.localScale += new Vector3(0.01f, 0.01f, 0.01f);
+    }
+
+    void shrinkObject(GameObject otherObject) {
+        otherObject.transform.localScale -= new Vector3(0.01f, 0.01f, 0.01f);
     }
 }

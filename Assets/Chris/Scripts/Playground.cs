45e6f576cbcd9ffa8ed56a392ba115d3e20f2bfc
aytona <christopher.aytona@gmail.com>
2017-02-22 11:03:26 -0500

    Photon part 1

--- Assets/Chris/Scripts/Playground.cs
+++ Assets/Chris/Scripts/Playground.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Networking {
-	public class Playground : MonoBehaviour {
-		#region Variables
+	public class Playground : Photon.PunBehaviour {
+        #region Variables
+        // The PUN loglevel
+        public PhotonLogLevel m_LogLevel = PhotonLogLevel.Informational;
+        // Maximum number of players per room
+        public byte m_MaxPlayers = 4;
+
 		// This client's version number. Users are separated from each other by gameversions
 		private string m_Version = "1";
 		#endregion
@@ -16,15 +21,49 @@ namespace Networking {
 			// This makes sure we can use PhotonNetwork.LoadLevel()
 			// on master client and all clients in the same room sync their level automatically
 			PhotonNetwork.automaticallySyncScene = true;
+            // Force Photon Log level
+            PhotonNetwork.logLevel = m_LogLevel;
 		}
 
 		void Start() {
-			Connect();	
+            Connect();
 		}
-		#endregion
+        #endregion
 
-		#region Public Methods
-		public void Connect() {
+        #region Photon.PunBehaviour Callbacks
+        /// <summary>
+        /// Called after the connection to the master is established and authenticated but only when PhotonNetwork.autoJoinLobby is false
+        /// </summary>
+        public override void OnConnectedToMaster() {
+            Debug.Log("OnConnectedToMaster was called by PUN");
+        }
+
+        /// <summary>
+        /// Called after disconnecting from server
+        /// </summary>
+        public override void OnDisconnectedFromPhoton() {
+            Debug.Log("OnDisconnectedFromPhoton was called by PUN");
+        }
+
+        /// <summary>
+        /// Called when CreateRoom() failed
+        /// </summary>
+        /// <param name="codeAndMsg">[0] is error code. [1] is error string msg</param>
+        public override void OnPhotonCreateRoomFailed(object[] codeAndMsg) {
+            Debug.Log("OnPhotonCreateRoomFailed was called by PUN");
+            PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = m_MaxPlayers }, null);
+        }
+
+        /// <summary>
+        /// Called when creating/joining a room. Called on all clients including master client
+        /// </summary>
+        public override void OnJoinedRoom() {
+            Debug.Log("OnJoinedRoom was callled by PUN");
+        }
+        #endregion
+
+        #region Public Methods
+        public void Connect() {
 			if (PhotonNetwork.connected) {
 				// Attempt to join a random room. If it fails, we'll get notified and create one
 				PhotonNetwork.JoinRandomRoom();
@@ -34,6 +73,6 @@ namespace Networking {
 				PhotonNetwork.ConnectUsingSettings(m_Version);
 			}
 		}
-		#endregion
-	}
+        #endregion
+    }
 }

b16d89d78dbdd3399ca93e974fb73bdacc8b3166
aytona <christopher.aytona@gmail.com>
2017-04-11 12:43:06 -0400

    GameManager associated with networking

---
+++ Assets/Chris/Scripts/NGameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// GameManager associated with Networking
+/// </summary>
+namespace Network {
+    public class NGameManager : SingletonClass<NGameManager> {
+        public GameObject m_Player;
+
+        private NetworkManager m_NetworkManager;
+
+        private void Awake() {
+            m_NetworkManager = FindObjectOfType<NetworkManager>();
+        }
+
+        private void Start() {
+            if (m_NetworkManager.currentState == NetworkManager._NetworkState.Single) {
+                m_Player.SetActive(true);
+            }
+        }
+    }
+}

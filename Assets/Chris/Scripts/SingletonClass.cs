c5ce94322b72e1ac42bd080ec14945567d7bdd78
aytona <christopher.aytona@gmail.com>
2017-04-11 12:40:56 -0400

    Singleton class

---
+++ Assets/Chris/Scripts/SingletonClass.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Singleton class that will make inherited class a singleton
+/// </summary>
+public class SingletonClass<T> : MonoBehaviour where T : MonoBehaviour {
+    private static T instance;
+
+    public static T Instance {
+        get {
+            if (instance == null) {
+                instance = FindObjectOfType<T>();
+                if (instance == null) {
+                    GameObject singleton = new GameObject(typeof(T).Name);
+                    instance = singleton.AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    public virtual void Awake() {
+        if (instance == null) {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+}

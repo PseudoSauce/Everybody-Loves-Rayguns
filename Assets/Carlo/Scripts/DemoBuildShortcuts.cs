01559157cabb74269bc416386358b3eba2e87df7
CarloAlbino <carloalbino@gmail.com>
2017-04-20 22:48:02 -0400

    Added a main menu for the presentation

--- Assets/Carlo/Scripts/DemoBuildShortcuts.cs
+++ Assets/Carlo/Scripts/DemoBuildShortcuts.cs
@@ -5,17 +5,50 @@ using UnityEngine.SceneManagement;
 
 public class DemoBuildShortcuts : MonoBehaviour {
 
+    public bool m_isMainMenu = false;
+    public int m_hubIndex = 1;
+    public int m_mainMenuIndex = 5;
+    public GameObject m_Instructions;
+
 	void Update ()
     {
-		if(Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.R))
+        if (m_isMainMenu)
         {
-            // Reset Game
-            SceneManager.LoadScene(0);
+            if (Input.GetKey(KeyCode.Return) || Input.GetKey(KeyCode.KeypadEnter))
+            {
+                // Start Game
+                SceneManager.LoadScene(m_hubIndex);
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.R))
+            {
+                // Reset Game
+                SceneManager.LoadScene(m_mainMenuIndex);
+            }
+
+            if(m_Instructions != null)
+            {
+                if (Input.GetKey(KeyCode.T))
+                {
+                    // Toggle Instructions
+                    if(m_Instructions.activeInHierarchy == true)
+                    {
+                        m_Instructions.SetActive(false);
+                    }
+                    else
+                    {
+                        m_Instructions.SetActive(true);
+                    }
+                }
+            }
         }
 
         if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.Escape))
         {
             // Quit Game
+            Debug.Log("Quitting...");
             Application.Quit();
         }
     }
